448d53c1c42023b3419411591a31db4c3b13302b
Guilherme Azevedo <COGTIT545@firjan.com.br>
2021-01-25 18:45:06 -0300

    Primeiro commit com o projeto para o github

---
+++ Rest_API_Teste/Context/AppContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rest_API_Teste.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rest_API_Teste.Context
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+    }
+}
---
+++ Rest_API_Teste/Controllers/PessoaController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Rest_API_Teste.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Rest_API_Teste.Controllers
+{
+    [Route("v1/pessoa")]
+    [ApiController]
+    public class PessoaController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Pessoa>> Post([FromServices] Context.AppContext context, [FromBody] Pessoa model)
+        {
+            if (ModelState.IsValid) 
+            {
+                context.Pessoas.Add(model);
+                await context.SaveChangesAsync();
+
+                return Created(nameof(model), "Criado com sucesso!");
+            }
+
+            return BadRequest();
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Pessoa>> Get([FromRoute] int id, [FromServices] Context.AppContext context)
+        {
+            var pessoas =  await context.Pessoas.Where(p=> p.PessoaId == id).FirstOrDefaultAsync();
+
+            if (pessoas != null)
+            { 
+                return Ok(pessoas);
+            }
+
+            return NotFound();
+        }
+    }
+}
